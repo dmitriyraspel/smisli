dd51cefbf218a66edb9917c4af1be957f62c741f
dmitriyraspel@yandex.ru <2905Tati>
2021-09-23 16:07:51 +0300

    fix functions.php

--- rspl_theme/functions.php
+++ rspl_theme/functions.php
@@ -50,7 +50,9 @@ if ( ! function_exists( 'rspl_theme_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus(
 			array(
-				'menu-1' => esc_html__( 'Primary', 'rspl_theme' ),
+				'primary'	=> __( 'Primary menu', 'rspl_theme' ),
+				'social'	=> __( 'Social Links menu', 'rspl_theme' ),
+				'footer'	=> __( 'Footer menu', 'rspl_theme' ),
 			)
 		);
 
@@ -61,60 +63,113 @@ if ( ! function_exists( 'rspl_theme_setup' ) ) :
 		add_theme_support(
 			'html5',
 			array(
-				'search-form',
 				'comment-form',
 				'comment-list',
 				'gallery',
 				'caption',
 				'style',
 				'script',
+				'navigation-widgets',
 			)
 		);
-
-		// Set up the WordPress core custom background feature.
-		add_theme_support(
-			'custom-background',
-			apply_filters(
-				'rspl_theme_custom_background_args',
-				array(
-					'default-color' => 'ffffff',
-					'default-image' => '',
-				)
-			)
-		);
-
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
 
 		/**
 		 * Add support for core custom logo.
 		 *
 		 * @link https://codex.wordpress.org/Theme_Logo
 		 */
+		$logo_width  = 300;
+		$logo_height = 100;
+
 		add_theme_support(
 			'custom-logo',
 			array(
-				'height'      => 250,
-				'width'       => 250,
-				'flex-width'  => true,
-				'flex-height' => true,
+				'height'               => $logo_height,
+				'width'                => $logo_width,
+				'flex-width'           => true,
+				'flex-height'          => true,
+				'unlink-homepage-logo' => true,
 			)
 		);
+
+		// Add theme support for selective refresh for widgets.
+		add_theme_support( 'customize-selective-refresh-widgets' );
+
+		// Add support for Block Styles.
+		// add_theme_support( 'wp-block-styles' );
+
+		// Add support for full and wide align images.
+		add_theme_support( 'align-wide' );
+
+		// Custom background color.
+		add_theme_support(
+			'custom-background',
+			array(
+				'default-color' => 'ffffff',
+			)
+		);
+
+		// Add support for responsive embedded content.
+		// add_theme_support( 'responsive-embeds' );
+
+		// Add support for custom line height controls.
+		// add_theme_support( 'custom-line-height' );
+
+		// Add support for experimental link color control.
+		add_theme_support( 'experimental-link-color' );
+
+		// Add support for experimental cover block spacing.
+		// add_theme_support( 'custom-spacing' );
+
+		// Add support for custom units.
+		// This was removed in WordPress 5.6 but is still required to properly support WP 5.5.
+		// add_theme_support( 'custom-units' );
 	}
 endif;
 add_action( 'after_setup_theme', 'rspl_theme_setup' );
 
 /**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
+ * Add Google webfonts.
  */
-function rspl_theme_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'rspl_theme_content_width', 640 );
+function rspl_theme_fonts_url() {
+
+	$fonts_url = '';
+
+	/* Translators: If there are characters in your language that are not
+	* supported by Roboto, translate this to 'off'. Do not translate
+	* into your own language.
+	*/
+	$roboto = esc_html_x( 'on', 'Roboto font: on or off', 'rspl_theme' );
+	// <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,400;0,700;1,100;1,400;1,700&display=swap" rel="stylesheet">
+	
+	/* Translators: If there are characters in your language that are not
+	* supported by Raleway, translate this to 'off'. Do not translate
+	* into your own language.
+	*/
+	$raleway = esc_html_x( 'off', 'Raleway font: on or off', 'rspl_theme' );
+	// <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,400;0,700;1,100;1,400;1,700&family=Roboto:ital,wght@0,100;0,400;0,700;1,100;1,400;1,700&display=swap" rel="stylesheet">
+
+	if ( 'off' !== $roboto || 'off' !== $raleway ) {
+		$font_families = array();
+
+		if ( 'off' !== $roboto ) {
+			$font_families[] = 'Roboto:ital,wght@0,100;0,400;0,700;1,100;1,400;1,700'; 
+		}
+
+		if ( 'off' !== $raleway ) {
+			$font_families[] = 'Raleway:ital,wght@0,100;0,400;0,700;1,100;1,400;1,700';
+		}
+
+		$query_args = array(
+			'family' => urlencode( implode( '|', $font_families ) ),
+			'subset' => urlencode( 'cyrillic-ext' ),
+		);
+
+		$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css2' );
+	}
+
+	return esc_url_raw( $fonts_url );
 }
-add_action( 'after_setup_theme', 'rspl_theme_content_width', 0 );
 
 /**
  * Register widget area.
@@ -122,6 +177,43 @@ add_action( 'after_setup_theme', 'rspl_theme_content_width', 0 );
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function rspl_theme_widgets_init() {
+
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Header', 'rspl_theme' ),
+			'id'            => 'sidebar-3',
+			'description'   => esc_html__( 'Add widgets here to appear in your header.', 'rspl_theme' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Footer', 'rspl_theme' ),
+			'id'            => 'sidebar-2',
+			'description'   => esc_html__( 'Add widgets here to appear in your footer.', 'rspl_theme' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Header-top', 'rspl_theme' ),
+			'id'            => 'sidebar-4',
+			'description'   => esc_html__( 'Add widgets here to appear in your before header.', 'rspl_theme' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title screen-reader-text">',
+			'after_title'   => '</h2>',
+		)
+	);
+
 	register_sidebar(
 		array(
 			'name'          => esc_html__( 'Sidebar', 'rspl_theme' ),
@@ -137,13 +229,35 @@ function rspl_theme_widgets_init() {
 add_action( 'widgets_init', 'rspl_theme_widgets_init' );
 
 /**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function rspl_theme_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'rspl_theme_content_width', 720 );
+}
+add_action( 'after_setup_theme', 'rspl_theme_content_width', 0 );
+
+
+/**
  * Enqueue scripts and styles.
  */
 function rspl_theme_scripts() {
-	wp_enqueue_style( 'rspl_theme-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'rspl_theme-style', 'rtl', 'replace' );
+	// Enqueue Google fonts
+	wp_enqueue_style( 'rspl_theme-fonts', rspl_theme_fonts_url(), array(), null );
 
-	wp_enqueue_script( 'rspl_theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	// wp_enqueue_style( 'rspl_theme-style', get_stylesheet_uri(), array(), _S_VERSION );
+	// wp_style_add_data( 'rspl_theme-style', 'rtl', 'replace' );
+
+	// Main style temp
+	wp_enqueue_style( 'rspl_theme-style-temp', get_template_directory_uri() . '/style.css', array(), filemtime(get_template_directory() . '/style.css') );
+
+	// wp_enqueue_script( 'rspl_theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+
+	// Main script temp
+	wp_enqueue_script( 'rspl_theme-script-temp', get_template_directory_uri() . 'assets/js/navigation.js', array(), filemtime(get_template_directory() . '/assets/js/navigation.js')  );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -167,6 +281,16 @@ require get_template_directory() . '/inc/template-tags.php';
 require get_template_directory() . '/inc/template-functions.php';
 
 /**
+ * SVG Icons class.
+ */
+require get_template_directory() . '/classes/class-architect-svg-icons.php';
+
+/**
+ * SVG Icons related functions.
+ */
+require get_template_directory() . '/inc/icon-functions.php';
+
+/**
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
@@ -174,13 +298,13 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
+// if ( defined( 'JETPACK__VERSION' ) ) {
+// 	require get_template_directory() . '/inc/jetpack.php';
+// }
 
 /**
  * Load WooCommerce compatibility file.
  */
-if ( class_exists( 'WooCommerce' ) ) {
-	require get_template_directory() . '/inc/woocommerce.php';
-}
+// if ( class_exists( 'WooCommerce' ) ) {
+// 	require get_template_directory() . '/inc/woocommerce.php';
+// }
