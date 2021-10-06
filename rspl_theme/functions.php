fb7e12a5a76538fe0d09b9225a4f25cc38bc420d
dmitriyraspel@yandex.ru <2905Tati>
2021-10-06 17:28:05 +0300

    fix text

--- rspl_theme/functions.php
+++ rspl_theme/functions.php
@@ -47,7 +47,7 @@ if ( ! function_exists( 'rspl_theme_setup' ) ) :
 		 */
 		add_theme_support( 'post-thumbnails' );
 
-		// This theme uses wp_nav_menu() in one location.
+		// This theme uses wp_nav_menu() in 3 location.
 		register_nav_menus(
 			array(
 				'primary'	=> __( 'Primary menu', 'rspl_theme' ),
