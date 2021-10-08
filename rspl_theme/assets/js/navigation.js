520fe721d158f96d61e4b86349caa43d81a97510
dmitriyraspel@yandex.ru <2905Tati>
2021-10-08 21:22:23 +0300

    add navigation.js

---
+++ rspl_theme/assets/js/navigation.js
@@ -0,0 +1,116 @@
+/**
+ * File navigation.js.
+ *
+ */
+
+function primary_nav() {
+	var siteNavigation = document.getElementById( 'site-navigation' );
+	var button = document.getElementById( 'primary-menu-toggle' );
+
+	// Return early if the navigation don't exist.
+	if (!siteNavigation) {
+		return;
+	}
+
+	if (!button) {
+		return;
+	}
+	// Toggle the .primary-navigation-open class and the aria-expanded value.
+	button.addEventListener( 'click', function () {
+		if (button.getAttribute( 'aria-expanded' ) === 'true' ) {
+			rsplthemeCloseSiteNav();
+		} else {
+			document.body.classList.add( 'primary-navigation-open' );
+			button.setAttribute( 'aria-expanded', 'true' );
+			setTimeout(function () {
+				document.addEventListener( 'click', rsplthemeCollapseSiteNavOnClickOutside )
+			}, 700);
+		}
+	});
+}
+
+/**
+ * Close if click outside Site-navigation
+ */
+function rsplthemeCollapseSiteNavOnClickOutside( event ) {
+	var siteNavigation = document.getElementById( 'site-navigation' );
+
+	if ( ! siteNavigation.contains( event.target ) ) {
+		rsplthemeCloseSiteNav();
+	}
+}
+
+/**
+ * Close opened Primary-navigation
+ */
+function rsplthemeCloseSiteNav() {
+	var button = document.getElementById( 'primary-menu-toggle' );
+
+	document.body.classList.remove("primary-navigation-open");
+	button.setAttribute( 'aria-expanded', 'false' );
+	document.removeEventListener( 'click', rsplthemeCollapseSiteNavOnClickOutside );
+}
+
+
+/**
+ * Close opened submenu.
+ */
+function rsplthemeCloseSubMenu( li ) {
+	if (! li.classList.contains( 'sub-menu-open' ) ) {
+		return;
+	} else {
+		li.classList.remove( 'sub-menu-open' );
+	}
+}
+
+/**
+ * Handle clicks on submenu button.
+ */
+function rsplthemeExpandSubMenu( el ) {
+	// Close other expanded items.
+	el.closest( 'nav' ).querySelectorAll( '.sub-menu-toggle' ).forEach( function( button ) {
+		if ( button !== el ) {
+			button.setAttribute( 'aria-expanded', 'false' );
+			rsplthemeCloseSubMenu(button.parentElement);
+		}
+	} );
+	// Toggle aria-expanded on the button.
+	rsplthemeToggleAriaExpanded( el, true );
+}
+
+/**
+ * Close if click outside current element Submenu
+ */
+ function rsplthemeCollapseSubmenuOnClickOutside( event ) {
+	if ( ! document.getElementById( 'site-navigation' ).contains( event.target ) ) {
+		document.getElementById( 'site-navigation' ).querySelectorAll( '.sub-menu-toggle' ).forEach( function( button ) {
+			button.setAttribute( 'aria-expanded', 'false' );
+			rsplthemeCloseSubMenu(button.parentElement);
+			document.removeEventListener( 'click', rsplthemeCollapseSubmenuOnClickOutside );
+		} );
+	}
+}
+
+/**
+ * Toggle an attribute's value
+ *
+ * @param {Element} el - The element.
+ * @param {boolean} withListeners - Whether we want to add/remove listeners or not.
+ */
+function rsplthemeToggleAriaExpanded( el, withListeners ) {
+	el.parentElement.classList.toggle( 'sub-menu-open' );
+
+	if ( 'true' !== el.getAttribute( 'aria-expanded' ) ) {
+		el.setAttribute( 'aria-expanded', 'true' );
+		if ( withListeners ) {
+			document.addEventListener( 'click', rsplthemeCollapseSubmenuOnClickOutside );
+		}
+	} else {
+		el.setAttribute( 'aria-expanded', 'false' );
+		if ( withListeners ) {
+			document.removeEventListener( 'click', rsplthemeCollapseSubmenuOnClickOutside );
+		}
+	}
+}
+
+primary_nav();
