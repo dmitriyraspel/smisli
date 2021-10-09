2738a1394c64cb77b8f55432b13f98c445eec272
dmitriyraspel@yandex.ru <2905Tati>
2021-10-09 18:15:49 +0300

    fix site-branding with customizer setting

--- rspl_theme/template-parts/header/site-branding.php
+++ rspl_theme/template-parts/header/site-branding.php
@@ -1,39 +1,68 @@
 <?php
 /**
- * Displays the site header.
+ * Displays the site header branding.
  *
  * @package RSPL-theme
  */
 
-$blog_info    = get_bloginfo( 'name' );
-$description  = get_bloginfo( 'description', 'display' );
-$show_title   = ( true === get_theme_mod( 'display_title_and_tagline', true ) );
-$header_class = $show_title ? 'site-title' : 'screen-reader-text';
-
+$blog_info          = get_bloginfo( 'name' );
+$description        = get_bloginfo( 'description', 'display' );
+$show_title         = ( true === get_theme_mod( 'display_site_title', true ) );
+$show_description   = ( true === get_theme_mod( 'display_site_tagline' ) );
+$title_class        = $show_title ? 'site-title' : 'screen-reader-text';
+$description_class  = $show_description ? 'site-description' : 'screen-reader-text';
 ?>
 
-<?php
-/* Show site logo */
-if (has_custom_logo()) : ?>
-  <div class="site-logo"><?php the_custom_logo(); ?></div>
-<?php endif; ?>
+<div class="site-branding">
 
-<div class="site-branding">  
   <?php
-  if ( is_front_page() || is_home() ) :
-    ?>
-    <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-    <?php
-  else :
-    ?>
-    <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-    <?php
-  endif;
+  /* Show only site logo without title & description */
+  if ( has_custom_logo() && ! $show_title && ! $show_description ) : ?>
+    <div class="site-logo"><?php the_custom_logo(); ?></div>
 
-  $rspl_theme_description = get_bloginfo( 'description', 'display' );
-  if ( $rspl_theme_description || is_customize_preview() ) :
-    ?>
-    <p class="site-description"><?php echo $rspl_theme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+    <?php if ( $blog_info ) : ?>
+      <?php if ( is_front_page() && ! is_paged() ) : ?>
+        <h1 class="<?php echo esc_attr( $title_class ); ?>"><?php echo esc_html( $blog_info ); ?></h1>
+      <?php elseif ( is_front_page() && ! is_home() ) : ?>
+        <h1 class="<?php echo esc_attr( $title_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo esc_html( $blog_info ); ?></a></h1>
+      <?php else : ?>
+        <p class="<?php echo esc_attr( $title_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo esc_html( $blog_info ); ?></a></p>
+      <?php endif; ?>
+	  <?php endif; ?>
+
+    <?php if ( $description ) : ?>
+      <p class="<?php echo esc_attr( $description_class ); ?>"><?php echo $description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+    <?php endif; ?>
+  
+  <?php
+  // Show site logo with title & description
+  else : ?>
+
+    <?php if ( has_custom_logo() ) : ?>
+      <div class="site-logo"><?php the_custom_logo(); ?></div>
+    <?php endif; ?>
+
+    <?php if ( has_custom_logo() && ( $show_title && $show_description ) ) : ?>
+      <div class="site-title-wrapper">
+    <?php endif; ?>
+
+      <?php if ( $blog_info ) : ?>
+        <?php if ( is_front_page() && ! is_paged() ) : ?>
+          <h1 class="<?php echo esc_attr( $title_class ); ?>"><?php echo esc_html( $blog_info ); ?></h1>
+        <?php elseif ( is_front_page() && ! is_home() ) : ?>
+          <h1 class="<?php echo esc_attr( $title_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo esc_html( $blog_info ); ?></a></h1>
+        <?php else : ?>
+          <p class="<?php echo esc_attr( $title_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo esc_html( $blog_info ); ?></a></p>
+        <?php endif; ?>
+      <?php endif; ?>
+
+      <?php if ( $description ) : ?>
+        <p class="<?php echo esc_attr( $description_class ); ?>"><?php echo $description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+      <?php endif; ?>
+
+      <?php if ( $show_title && $show_description ) : ?>
+      </div><!-- /.site-title-wrapper -->
+      <?php endif; ?>
+
   <?php endif; ?>
-
-</div><!-- .site-branding -->
+</div><!-- /.site-branding -->
--- rspl_theme/template-parts/header/site-header.php
+++ rspl_theme/template-parts/header/site-header.php
@@ -6,6 +6,8 @@
  */
 $wrapper_classes  = 'site-header';
 $wrapper_classes .= has_custom_logo() ? ' has-logo' : '';
+$wrapper_classes .= ( true === get_theme_mod( 'display_site_title', true ) ) ? ' has-site-title' : '';
+$wrapper_classes .= ( true === get_theme_mod( 'display_site_tagline', true ) ) ? ' has-site-tagline' : '';
 $wrapper_classes .= is_active_sidebar( 'sidebar-4' ) ? ' has-header-top' : '';
 $wrapper_classes .= is_active_sidebar( 'sidebar-3' ) ? ' has-header-widget-area' : '';
 $wrapper_classes .= has_nav_menu( 'primary' ) ? ' has-primary-menu' : '';
@@ -18,9 +20,7 @@ $wrapper_classes .= has_nav_menu( 'primary' ) ? ' has-primary-menu' : '';
 
 	<div class="container header-inner">
 
-		<div class="header-inner-left">
-			<?php get_template_part( 'template-parts/header/site-branding' ); ?>
-		</div><!-- /.header-inner-left -->
+		<?php get_template_part( 'template-parts/header/site-branding' ); ?>
 
 		<button id="primary-menu-toggle" class="menu-toggle" aria-controls="primary-menu-list" aria-expanded="false">
       <span class="screen-reader-text"><?php esc_html_e( 'Primary Menu', 'rspl_theme' ); ?></span>
