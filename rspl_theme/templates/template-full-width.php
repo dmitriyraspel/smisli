dd261e159a9dfab551aa41bc1dd32fcc74243ea0
dmitriyraspel@yandex.ru <2905Tati>
2021-10-12 16:25:54 +0300

    add template-full-width

---
+++ rspl_theme/templates/template-full-width.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: Full Width Template
+ * Template Post Type: page
+ * 
+ * @link https://developer.wordpress.org/themes/template-files-section/page-template-files/
+ *
+ * @package rspl-theme
+ */
+
+get_template_part( 'page' );
