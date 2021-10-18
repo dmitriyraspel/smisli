5d48eeee0380c437d57ab2f6b471e12ddd4bb5a4
dmitriyraspel@yandex.ru <2905Tati>
2021-10-18 12:38:29 +0300

    change site-footer

--- rspl_theme/footer.php
+++ rspl_theme/footer.php
@@ -16,39 +16,39 @@ $footer_wrapper_classes .= has_nav_menu( 'footer' ) ? ' has-footer-menu' : '';
 
 	<footer id="colophon" class="<?php echo esc_attr( $footer_wrapper_classes ); ?>">
 
-	<?php
-		if ( has_nav_menu( 'footer' ) ) : 
-			get_template_part( 'template-parts/navigation/footer-nav' );
-		endif; 
-	?>
+		<div class="container">
 
-	<?php
-		if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
-			<div class="widget-area container footer-widget-area">
-				<?php dynamic_sidebar( 'sidebar-2' ); ?>
-			</div><!-- .footer-widget-area -->
-		<?php endif; ?>
+				<?php
+				if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+					<div class="widget-area footer-widget-area">
+						<?php dynamic_sidebar( 'sidebar-2' ); ?>
+					</div><!-- .footer-widget-area -->
+				<?php endif; ?>
 
-	<?php
-		if ( has_nav_menu( 'social' ) ) : 
-			get_template_part( 'template-parts/navigation/social-nav' );
-		endif; 
-	?>
 
-	<div class="container site-info">
+				<?php
+				if ( has_nav_menu( 'social' ) ) : 
+					get_template_part( 'template-parts/navigation/social-nav' );
+				endif; 
+				?>
+			
+		</div><!-- /.container -->
+
+		<div class="site-info">
 			
 			<span class="site-name"><?php echo get_bloginfo( 'name' ); ?></span>
 			<span class="copyright">&copy; <?php echo esc_html( date_i18n( __( 'Y', 'rspl_theme' ) ) ); ?></span>
+			
 			<?php
-			if ( function_exists( 'the_privacy_policy_link' ) ) {
-				the_privacy_policy_link();
-			}
+			//if ( function_exists( 'the_privacy_policy_link' ) ) {
+			//	the_privacy_policy_link();
+			//}
 			?>
 
 			<span class="sep"> | </span>
 			<?php
 			/* translators: 1: Theme name, 2: Theme author. */
-			printf( esc_html__( 'RSPL_theme by %2$s', 'rspl_theme' ), 'rspl_theme', '<a class="theme-author" href="http://raspel.ru">Raspel</a>' );
+			printf( esc_html__( 'By %2$s', 'rspl_theme' ), 'rspl_theme', '<a class="theme-author" href="http://raspel.ru">Raspel</a>' );
 			?>
 			
 		</div><!-- .site-info -->
