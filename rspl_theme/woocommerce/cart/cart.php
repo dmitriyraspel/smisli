f4d992110f0585c5c204b1b16a06a3546c79dcef
dmitriyraspel@yandex.ru <2905Tati>
2022-12-14 22:06:11 +0300

    fix cart - coupon

--- rspl_theme/woocommerce/cart/cart.php
+++ rspl_theme/woocommerce/cart/cart.php
@@ -96,12 +96,8 @@ do_action( 'woocommerce_before_cart' ); ?>
                 ?>
               </a>
 
-              
             </div>
 
-             
-
-              
             </div> <!-- woocommerce-cart-form__cart-item -->
             <?php
           }
@@ -109,35 +105,25 @@ do_action( 'woocommerce_before_cart' ); ?>
         ?>
   </div>
 
-
-	
-  <table class="shop_table shop_table_responsive cart woocommerce-cart-form__contents" cellspacing="0">
-		
-		<tbody>
-			<?php // do_action( 'woocommerce_before_cart_contents' ); ?>
-
-			
-
-			<?php do_action( 'woocommerce_cart_contents' ); ?>
-
-			<tr>
-				<td colspan="6" class="actions">
-
-					<?php if ( wc_coupons_enabled() ) { ?>
-						<div class="coupon">
+          <?php if ( wc_coupons_enabled() ) { ?>
+						<div class="coupon cart-coupon">
 							<label for="coupon_code"><?php esc_html_e( 'Coupon:', 'woocommerce' ); ?></label> <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" /> <button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?></button>
 							<?php do_action( 'woocommerce_cart_coupon' ); ?>
 						</div>
 					<?php } ?>
-
-					<button type="submit" class="button" name="update_cart" value="<?php esc_attr_e( 'Update cart', 'woocommerce' ); ?>"><?php esc_html_e( 'Update cart', 'woocommerce' ); ?></button>
-
-					<?php do_action( 'woocommerce_cart_actions' ); ?>
-
 					<?php wp_nonce_field( 'woocommerce-cart', 'woocommerce-cart-nonce' ); ?>
+
+
+	
+  <table class="shop_table shop_table_responsive cart woocommerce-cart-form__contents no-border" cellspacing="0">
+		<tbody>
+			<?php do_action( 'woocommerce_before_cart_contents' ); ?>
+			<?php do_action( 'woocommerce_cart_contents' ); ?>
+			<tr>
+				<td colspan="6" class="actions">
+					<?php do_action( 'woocommerce_cart_actions' ); ?>
 				</td>
 			</tr>
-
 			<?php do_action( 'woocommerce_after_cart_contents' ); ?>
 		</tbody>
 	</table>
