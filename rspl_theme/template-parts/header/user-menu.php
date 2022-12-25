4678551060d694c39091cca3438335a743fb5e20
dmitriyraspel@yandex.ru <2905Tati>
2022-12-25 19:48:16 +0300

    fix links user-menu

--- rspl_theme/template-parts/header/user-menu.php
+++ rspl_theme/template-parts/header/user-menu.php
@@ -9,7 +9,7 @@ if ( !is_user_logged_in() ) {
   ?>
   <div class="user-menu__wrap">
     <span class="user-menu__avatar"></span>
-    <a href="#" class="user-menu-login__link"></a>
+    <a href="<?php echo home_url( '/login' ) ?>" class="user-menu-login__link"></a>
   </div>
 <?php }
 else {
@@ -22,33 +22,33 @@ else {
           <ul class="user-menu__list">
           
             <li class="user-menu__item">
-              <a href="#" class="user-menu__link">
+              <a href="<?php echo home_url( '/lk' ) ?>" class="user-menu__link">
                 <?php echo __( 'Кабинет', 'rspl_theme' ) ?>
               </a>
             </li>
             <li class="user-menu__item">
-              <a href="#" class="user-menu__link">
+              <a href="<?php echo home_url( '/katalog-produktov' ) ?>" class="user-menu__link">
                 <?php echo __( 'Каталог продуктов', 'rspl_theme' ) ?>
               </a>
             </li>
             <li class="user-menu__item">
-              <a href="#" class="user-menu__link">
+              <a href="<?php echo home_url( '/izbrannoe' ) ?>" class="user-menu__link">
                 <?php echo __( 'Избранное', 'rspl_theme' ) ?>
               </a>
             </li>
             <li class="user-menu__item">
-              <a href="<?php wc_get_cart_url() ?>" class="user-menu__link">
+              <a href="<?php echo esc_url( wc_get_cart_url() ); ?>" class="user-menu__link">
                <?php echo __( 'Корзина', 'rspl_theme' ) ?>
               </a>
             </li>
             <li class="user-menu__item">
-              <a href="#" class="user-menu__link">
+              <a href="<?php echo home_url( '/profil' ) ?>" class="user-menu__link">
                 <?php echo __( 'Профиль', 'rspl_theme' ) ?>
               </a>
             </li>
 
             <li class="user-menu__item">
-              <a href="<?php wp_logout_url( home_url( '/my-courses' ) ) ?>" class="user-menu__link user-menu__link user-menu__logout">
+              <a href="<?php echo wp_logout_url( home_url( '/my-courses' ) ) ?>" class="user-menu__link user-menu__link user-menu__logout">
                 <?php echo __( 'Выход', 'rspl_theme' ) ?>
                 
               </a>
