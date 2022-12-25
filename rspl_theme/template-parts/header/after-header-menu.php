44d67c36b5439ccf643252ed1f1c98fdcbcc571b
dmitriyraspel@yandex.ru <2905Tati>
2022-12-25 19:47:38 +0300

    fix links after-header-menu

--- rspl_theme/template-parts/header/after-header-menu.php
+++ rspl_theme/template-parts/header/after-header-menu.php
@@ -11,15 +11,15 @@ if ( !is_user_logged_in() ) {
   <nav class="after-header-menu__wrap after-header-menu_not-authorized">
     <ul class="after-header-menu__list">
       <li class="after-header-menu__item">
-        <a href="<?php echo home_url( '/my-courses' ) ?>" class="after-header-menu__link">
+        <a href="<?php echo home_url( '/lk' ) ?>" class="after-header-menu__link">
         <span class="after-header-menu__icon after-header-menu__icon-user"></span>
-          Вход в личный кабинет
+        <?php echo __( 'Вход в личный кабинет', 'rspl_theme' ) ?>
         </a>
       </li>
       <li class="after-header-menu__item">
         <a href="<?php echo home_url( '/katalog-produktov' ) ?>" class="after-header-menu__link">
         <span class="after-header-menu__icon after-header-menu__icon-tree"></span>
-          Каталог продуктов
+        <?php echo __( 'Каталог продуктов', 'rspl_theme' ) ?>
         </a>
       </li>   
     </ul>
@@ -33,33 +33,33 @@ else {
   <nav class="after-header-menu__wrap after-header-menu_authorized">
     <ul class="after-header-menu__list">
       <li class="after-header-menu__item">
-        <a href="<?php echo home_url( 'my-courses' ) ?>" class="after-header-menu__link">
+        <a href="<?php echo home_url( '/lk' ) ?>" class="after-header-menu__link">
           <span class="after-header-menu__icon after-header-menu__icon-user"></span>
-          Кабинет
+          <?php echo __( 'Кабинет', 'rspl_theme' ) ?>
         </a>
       </li>
       <li class="after-header-menu__item">
         <a href="<?php echo home_url( '/katalog-produktov' ) ?>" class="after-header-menu__link">
           <span class="after-header-menu__icon after-header-menu__icon-tree"></span>
-          Каталог продуктов
+          <?php echo __( 'Каталог продуктов', 'rspl_theme' ) ?>
         </a>
       </li>
       <li class="after-header-menu__item">
         <a href="<?php echo home_url( '/izbrannoe' ) ?>" class="after-header-menu__link">
         <span class="after-header-menu__icon after-header-menu__icon-fav "></span>
-          Избранное
+          <?php echo __( 'Избранное', 'rspl_theme' ) ?>
         </a>
       </li>
       <li class="after-header-menu__item">
         <a href="<?php echo esc_url( wc_get_cart_url() ); ?>" class="after-header-menu__link">
         <span class="after-header-menu__icon after-header-menu__icon-cart"></span>
-          Корзина
+          <?php echo __( 'Корзина', 'rspl_theme' ) ?>
         </a>
       </li>
       <li class="after-header-menu__item">
         <a href="<?php echo home_url( '/profil' ) ?>" class="after-header-menu__link">
           <span class="after-header-menu__icon after-header-menu__icon-gear"></span>
-          Профиль
+          <?php echo __( 'Профиль', 'rspl_theme' ) ?>
         </a>
       </li>
     </ul>
